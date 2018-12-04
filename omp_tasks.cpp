5a8f9698fc9418b51d96676410dfd3194b3f5c22
381706-1VlasovAndrey <andrio.vlasov.2000@mail.ru>
2018-12-05 02:46:35 +0400

    Quick Sort

--- omp_tasks.cpp
+++ omp_tasks.cpp
@@ -1,8 +1,9 @@
 #include <omp.h>
 #include <algorithm>
-#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
 
-void QuickSort(int* a, const long n) {
+void QuickSort(int* a, const int n) {
   int i = 0, j = n;
   int pivot = a[n / 2];
   do {
@@ -13,11 +14,11 @@ void QuickSort(int* a, const long n) {
       i++; j--;
     }
   } while (i <= j);
-  if (j > 0) quickSort(a, j);
-  if (n > i) quickSort(a + i, n - i);
+  if (j > 0) QuickSort(a, j);
+  if (n > i) QuickSort(a + i, n - i);
 }
 
-void OMP_QuickSort(int* a, const long n) {
+void OMP_QuickSort(int* a, const int n) {
 	int i = 0, j = n;
 	int pivot = a[n / 2];
 	
@@ -33,20 +34,20 @@ void OMP_QuickSort(int* a, const long n) {
 
 #pragma omp task shared(a)
 	{
-		if (j > 0) quickSort(a, j);
+		if (j > 0) OMP_QuickSort(a, j);
 	} 
 #pragma omp task shared(a)
 	{
-		if (n > i) quickSort(a + i, n - i);
+		if (n > i) OMP_QuickSort(a + i, n - i);
 	} 
 #pragma omp taskwait
 }
 
 
-int main(int argc, char *argv[]) {
-	int *a,b;
+int main(int argc, char** argv) {
+	int *a,*b;
 	double t1, t2, t3;
-	int n = 10000000;
+	int n = atoi (argv[1]);
 	a = new int[n];
 	b = new int[n];
 	for (int i = 0; i < n; ++i){
@@ -55,7 +56,7 @@ int main(int argc, char *argv[]) {
 	}
 	// последовательная
 	t1 = omp_get_wtime();
-	QuickSort(a, n - 1);
+	QuickSort(b, n - 1);
 	t2 = omp_get_wtime();
 	// последовательная
 	
@@ -63,7 +64,7 @@ int main(int argc, char *argv[]) {
 	{
 		#pragma omp single nowait 
 		{
-			OMP_QuickSort(b, n - 1);
+			OMP_QuickSort(a, n - 1);
 		} 
 	} 
 
